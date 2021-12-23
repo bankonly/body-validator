5a541ed3c3400e774e13d6ef2f759f738e105ffa
bankonly <for21bn01@gmail.com>
2021-12-24 00:41:47 +0700

    firstupdate

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const first_rule_allow = ["required", "optional"]
-const validate = ({ rule, body, exclude_body = false }) => {
+const _mongoose = require("mongoose")
+
+const first_rule_allow = ["required", "optional", "objectid"]
+const thrid_rule_allow = ["exist"]
+const validate = async ({ rule, body, exclude_body = false }) => {
+
     let _body = {}
     const rules_key = Object.keys(rule)
     for (let i = 0; i < rules_key.length; i++) {
@@ -7,17 +11,36 @@ const validate = ({ rule, body, exclude_body = false }) => {
         const rule_data = rules[rule_key]
 
         const split_rule_msg = rule_data.split("|")
-        if (split_rule_msg.length !== 2) throw new Error("Invalid rule expect xx|xx")
+        if (split_rule_msg.length < 2) throw new Error("Invalid rule expect xx|xx")
 
         const first_rule = split_rule_msg[0]
         const second_rule = split_rule_msg[1]
+        let third_rule = ""
+
+        if (split_rule_msg.length === 3) {
+            third_rule = split_rule_msg[2].split(":")
+            if (!thrid_rule_allow.includes(third_rule[0])) {
+                throw new Error(`Invalid thrid rule ${thrid_rule_allow}`)
+            }
+        }
+
 
         if (!first_rule_allow.includes(first_rule)) {
             throw new Error(`Invalid first rule ${first_rule_allow}`)
         }
 
-        if (first_rule === "required" && !body[rule_key]) {
+        if ((first_rule === "required" || first_rule === "objectid") && !body[rule_key]) {
             throw new Error(`400-${second_rule}`)
+        }
+
+        if (first_rule === "objectid" && !body[rule_key].toString().match(/^[0-9a-fA-F]{24}$/)) {
+            throw new Error(`400-${second_rule}OBJ`)
+        }
+
+        if (third_rule[0] === "exist" && !third_rule[1]) throw new Error(`exist rule required model name`)
+        if (third_rule[0] === "exist") {
+            const exist = await _mongoose.model(third_rule[1]).findOne({ [`${rule_key}`]: body[rule_key], deleted_at: null })
+            if (exist) throw new Error(`400-${third_rule[1].toUpperCase()}4000}`)
         }
 
         _body[rule_key] = body[rule_key]
@@ -25,5 +48,7 @@ const validate = ({ rule, body, exclude_body = false }) => {
 
     if (!exclude_body) return body
     return _body
+
 }
+
 module.exports = validate
