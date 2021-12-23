c55475a5425a1a3bdcfbbaf1677e9e3a2b7ab77c
bankonly <for21bn01@gmail.com>
2021-12-24 01:02:52 +0700

    firstupdate

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const validate = async ({ rule, body, exclude_body = false }) => {
 
     let _body = {}
     const rules_key = Object.keys(rule)
+    if (rules_key.length === 0) throw new Error(`No rule given`)
     for (let i = 0; i < rules_key.length; i++) {
-        const rule_key = rules_key[i];
+        let rule_key = rules_key[i];
         const rule_data = rule[rule_key]
+
+
+        const split_rule_key = rule_key.split(":")
+        if (split_rule_key.length > 1) {
+            rule_key = split_rule_key[0]
+        }
 
         const split_rule_msg = rule_data.split("|")
         if (split_rule_msg.length < 2) throw new Error("Invalid rule expect xx|xx")
@@ -39,8 +46,13 @@ const validate = async ({ rule, body, exclude_body = false }) => {
 
         if (third_rule[0] === "exist" && !third_rule[1]) throw new Error(`exist rule required model name`)
         if (third_rule[0] === "exist") {
-            const exist = await _mongoose.model(third_rule[1]).findOne({ [`${rule_key}`]: body[rule_key], deleted_at: null })
-            if (exist) throw new Error(`400-${third_rule[1].toUpperCase()}4000`)
+            let target_key = rule_key
+            if (split_rule_key.length > 1) {
+                target_key = split_rule_key[1]
+            }
+
+            const exist = await _mongoose.model(third_rule[1]).findOne({ [`${target_key}`]: body[rule_key], deleted_at: null })
+            if (exist) throw new Error(`400-${third_rule[1].toUpperCase()}4000}`)
         }
 
         _body[rule_key] = body[rule_key]
